05ebea8b95e20069047d92c38ef55b3857451cd7
Matheus Santos <matheusantos99@gmail.com>
2022-09-08 21:09:30 -0300

    background gradiente + formas e posicionamento

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -5,14 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,17 +37,27 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column {
+    Column(
+        Modifier
+            .width(200.dp)
+            .height(250.dp)
+    ) {
         Box(
             modifier = Modifier
-                .height(50.dp)
-                .width(50.dp)
-                .background(Color.Blue)
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(brush = Brush.verticalGradient(colors = listOf(
+                    Color.Red, Color.White, Color.Black
+                )))
         )
 
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null
+            contentDescription = null,
+            Modifier
+                .offset(y = (-50).dp)
+                .align(CenterHorizontally)
+                .clip(CircleShape)
         )
 
         Text(text = "Texto 1")
